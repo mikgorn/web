5b3a4c9dd543e143d4794fe5c9a514b06c881496
mikgorn <kirill_777_07@inbox.ru>
2017-12-01 22:22:30 +0900

    Modified header, added links

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <header class = "container-fluid">
     <div class = "row">
       <nav class = "col-lg-2 col-xs-2  text-left" >
-         <p>MAIN</p>
+         <p ><a class="menu" href="job-list.php">MAIN</a></p>
       </nav>
       <nav class="col-lg-2 "></nav>
       <nav class="col-lg-8 col-xs-10 text-right">
-        <p>LOG IN</p>
-        <p>SIGN UP</p>
+        <p ><a class="menu" href="login.php">LOG IN</a></p>
+        <p><a class="menu" href="signup.php">SIGN UP</a></p>
       	<p>CONTACT</p>
       </nav>
     </div>
